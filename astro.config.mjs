9c5404901b5d6922c47fa219bad2d5877b336362
Luis Enrique <enriquecalero284@gmail.com>
2024-10-17 13:06:40 -0500

    Node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  
+    output: 'hybrid',
+    adapter: node({
+      mode: 'middleware',
+    }),
+
+    vite: {
+        ssr: {
+          noExternal: true
+        }
+      }
   
 });
